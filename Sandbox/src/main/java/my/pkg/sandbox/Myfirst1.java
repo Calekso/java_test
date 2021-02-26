5f1c2338bfeb3ceafc643fee9e97b9dac36c9e4b
atsvetinsky <hmmm@nm.ru>
2021-02-26 23:24:01 +0300

    Изменены настройки, переименован класс и отформатирован код
    Signed-off-by: atsvetinsky <hmmm@nm.ru>

--- Sandbox/src/main/java/my/pkg/sandbox/Myfirst1.java
+++ Sandbox/src/main/java/my/pkg/sandbox/Myfirst1.java
@@ -1,9 +1,9 @@
 package my.pkg.sandbox;
 
-public class Myfirst1 {
+public class MyFirst1 {
 
 	public static void main(String[] args){
-		System.out.println("Hellow, world!");
+		System.out.println("Hello, world!");
 	}
 	
 }
