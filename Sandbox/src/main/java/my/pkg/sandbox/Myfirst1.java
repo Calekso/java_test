e136aa3630ccccd347fbecd8aab2d6e66275e0bc
atsvetinsky <hmmm@nm.ru>
2021-02-27 00:20:23 +0300

    Добавлены первые функции
    Signed-off-by: atsvetinsky <hmmm@nm.ru>

--- Sandbox/src/main/java/my/pkg/sandbox/Myfirst1.java
+++ Sandbox/src/main/java/my/pkg/sandbox/Myfirst1.java
@@ -2,8 +2,28 @@ package my.pkg.sandbox;
 
 public class MyFirst1 {
 
-	public static void main(String[] args){
-		System.out.println("Hello, world!");
-	}
-	
+  public static void main(String[] args) {
+
+    hello("World");
+    hello("user");
+
+    int l = 6;
+
+    int a = 2;
+    int b= 3;
+
+    System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+    System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b +  " = " + area(a, b));
+  }
+
+  public static void hello(String name) {
+    System.out.println("Hello, " + name + "!");
+  }
+
+  public static double area(double ars) {
+    return ars * ars;
+  }
+  public static double area(double ars1, double ars2) {
+    return ars1 * ars2;
+  }
 }
