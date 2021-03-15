4e0b3d3fe3ae57c9bd0a7738b56e1672fcfcdf66
atsvetinsky <hmmm@nm.ru>
2021-03-15 23:05:07 +0300

    Добавлена поддержка вызова в разных браузерах. Опробовано сравнение объектов с equals вместо ==

--- addressbook-web-test/src/test/java/my/pkg/addresbook/appmanager/AppManager.java
+++ addressbook-web-test/src/test/java/my/pkg/addresbook/appmanager/AppManager.java
@@ -2,43 +2,62 @@ package my.pkg.addresbook.appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
 public class AppManager {
-  public WebDriver wd;
+    public WebDriver wd;
 
-  private  SessionHelper sessionHelper;
-  private  NavigationHelper navigationHelper;
-  private  GroupHelper groupHelper;
-  private ContactHelper contactHelper;
+    private SessionHelper sessionHelper;
+    private NavigationHelper navigationHelper;
+    private GroupHelper groupHelper;
+    private ContactHelper contactHelper;
+    private String browser;
 
-  public void init() {
-    wd = new FirefoxDriver();
-    wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-    groupHelper = new GroupHelper(wd);
-    navigationHelper = new NavigationHelper(wd);
-    sessionHelper = new SessionHelper(wd);
-    contactHelper = new ContactHelper(wd);
-    sessionHelper.login("admin", "secret");
-  }
+    public AppManager(String browser) {
+
+        this.browser = browser;
+    }
+
+    public void init() { //equals - сравнение объектов, строк...
+        if (browser.equals(BrowserType.FIREFOX)) {
+            wd = new FirefoxDriver();
+        } else if (browser.equals(BrowserType.CHROME)) {
+            wd = new ChromeDriver();
+        } else if (browser.equals(BrowserType.IEXPLORE)) {
+            wd = new InternetExplorerDriver();
+        } else {
+            System.out.println("Unknown browser, use FireFox");
+            wd = new FirefoxDriver();
+        }
+
+        wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+        groupHelper = new GroupHelper(wd);
+        navigationHelper = new NavigationHelper(wd);
+        sessionHelper = new SessionHelper(wd);
+        contactHelper = new ContactHelper(wd);
+        sessionHelper.login("admin", "secret");
+    }
 
 
-  public void stop() {
-    sessionHelper.clickElem(By.linkText("Logout"));
-    wd.quit();
-  }
+    public void stop() {
+        sessionHelper.clickElem(By.linkText("Logout"));
+        wd.quit();
+    }
 
-  public GroupHelper getGroupHelper() {
-    return groupHelper;
-  }
+    public GroupHelper getGroupHelper() {
+        return groupHelper;
+    }
 
-  public ContactHelper getContactHelper() {
-    return contactHelper;
-  }
+    public ContactHelper getContactHelper() {
+        return contactHelper;
+    }
 
-  public NavigationHelper getNavigationHelper() {
-    return navigationHelper;
-  }
+    public NavigationHelper getNavigationHelper() {
+        return navigationHelper;
+    }
 }
--- addressbook-web-test/src/test/java/my/pkg/addresbook/tests/TestBase.java
+++ addressbook-web-test/src/test/java/my/pkg/addresbook/tests/TestBase.java
@@ -1,12 +1,13 @@
 package my.pkg.addresbook.tests;
 
 import my.pkg.addresbook.appmanager.AppManager;
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 public class TestBase {
 
-  protected final AppManager app = new AppManager();
+  protected final AppManager app = new AppManager(BrowserType.IEXPLORE);
 
   @BeforeMethod(alwaysRun = true)
   public void setUp() throws Exception {
