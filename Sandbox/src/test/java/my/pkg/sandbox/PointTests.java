b13b67a0efe6db5cf2bbf193448d5b6cdd25f54c
atsvetinsky <hmmm@nm.ru>
2021-05-08 12:33:53 +0300

    Bad test for Jankins

--- Sandbox/src/test/java/my/pkg/sandbox/PointTests.java
+++ Sandbox/src/test/java/my/pkg/sandbox/PointTests.java
@@ -5,7 +5,7 @@ import org.testng.annotations.Test;
 
 public class PointTests {
 
-  @Test(enabled = false)
+  @Test(enabled = true)
   public void testPointFirst(){
     Point p1 = new Point(4,0);
     Point p2 = new Point(4,3);
