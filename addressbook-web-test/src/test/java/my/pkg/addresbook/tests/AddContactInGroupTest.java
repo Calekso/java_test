6c59f8dca20af3a74cb82118782ef6cd94b9446d
atsvetinsky <hmmm@nm.ru>
2021-05-08 22:53:43 +0300

    Добавлен метод поиска контакта без группы + предусловие.

--- addressbook-web-test/src/test/java/my/pkg/addresbook/tests/AddContactInGroupTest.java
+++ addressbook-web-test/src/test/java/my/pkg/addresbook/tests/AddContactInGroupTest.java
@@ -4,6 +4,7 @@ import my.pkg.addresbook.model.ContactData;
 import my.pkg.addresbook.model.Contacts;
 import my.pkg.addresbook.model.GroupData;
 import my.pkg.addresbook.model.Groups;
+import org.hamcrest.CoreMatchers;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -15,12 +16,24 @@ public class AddContactInGroupTest extends TestBase {
     @BeforeMethod
     public void ensurePreconditions(){
         if (app.db().contacts().size() == 0) {
-            app.contact().createContact(new ContactData().withName("test1").withLastName("test2").withMiddleName("test3"));
+            app.contact().createContact(new ContactData()
+                    .withName("test1")
+                    .withLastName("test2")
+                    .withMiddleName("test3"));
+            app.goTo().goToHomePage();
         }
         if (app.db().groups().size() == 0) {
             app.goTo().groupPage();
             app.group().create(new GroupData().withName("test1"));
+            app.goTo().goToHomePage();
 
+        }
+        if (app.contact().сontactWithoutGroup(app.db().contacts()) == null) {
+            app.contact().createContact(new ContactData()
+                    .withName("test1")
+                    .withLastName("test2")
+                    .withMiddleName("test3"));
+            app.goTo().goToHomePage();
         }
     }
 
@@ -31,13 +44,16 @@ public class AddContactInGroupTest extends TestBase {
         app.goTo().goToHomePage();
         Groups groups = app.db().groups();
         GroupData group = groups.iterator().next();
-        ContactData contact = before.iterator().next();
+        ContactData contact =  app.contact().сontactWithoutGroup(before);
         ContactData contactAddedGroup = contact.inGroup(group);
 
-        app.contact().addToGroup(contactAddedGroup);
+        app.contact().addToGroup(contactAddedGroup, group);
 
         Contacts after = app.db().contacts();
         assertThat(after.size(), equalTo(before.size()));
-        assertThat(after, equalTo(before.Without(contact).WithAdded(contactAddedGroup)));
+
+        Contacts contacts_after = app.db().getContactById(contact.getId());
+        ContactData contactWithGroup = contacts_after.iterator().next();
+        assertThat(contactWithGroup, CoreMatchers.equalTo(contactAddedGroup));
     }
 }
