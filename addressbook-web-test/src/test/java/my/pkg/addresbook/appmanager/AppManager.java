04e55fa49ebab2941cfd7a8621ccf87776bb94f5
atsvetinsky <hmmm@nm.ru>
2021-05-08 13:16:21 +0300

    Selenium server test Start

--- addressbook-web-test/src/test/java/my/pkg/addresbook/appmanager/AppManager.java
+++ addressbook-web-test/src/test/java/my/pkg/addresbook/appmanager/AppManager.java
@@ -1,16 +1,21 @@
 package my.pkg.addresbook.appmanager;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
@@ -35,15 +40,21 @@ public class AppManager {
         properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
         dbHelper = new DbHelper();
 
-        if (browser.equals(BrowserType.FIREFOX)) {
-            wd = new FirefoxDriver();
-        } else if (browser.equals(BrowserType.CHROME)) {
-            wd = new ChromeDriver();
-        } else if (browser.equals(BrowserType.IEXPLORE)) {
-            wd = new InternetExplorerDriver();
+        if("".equals(properties.getProperty("selenium.server"))) {
+            if (browser.equals(BrowserType.FIREFOX)) {
+                wd = new FirefoxDriver();
+            } else if (browser.equals(BrowserType.CHROME)) {
+                wd = new ChromeDriver();
+            } else if (browser.equals(BrowserType.IEXPLORE)) {
+                wd = new InternetExplorerDriver();
+            } else {
+                System.out.println("Unknown browser, use FireFox");
+                wd = new FirefoxDriver();
+            }
         } else {
-            System.out.println("Unknown browser, use FireFox");
-            wd = new FirefoxDriver();
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setBrowserName(browser);
+            wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);
         }
 
         wd.manage().timeouts().implicitlyWait(6, TimeUnit.SECONDS);
